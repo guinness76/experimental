b39fcef2b081177ed356ebd36d0d57a9d1664ee2
Matt Gross <zoidberg_76@yahoo.com>
2018-03-16 15:37:05 -0700

    Added MultiMap example

--- src/main/java/org/experimental/Playground.java
+++ src/main/java/org/experimental/Playground.java
@@ -6,18 +6,6 @@ package org.experimental;
 public class Playground {
 
     public static void main(String[] args) throws Exception {
-        String cfgStr = "{\n"
-            + "  \"values\": {\n"
-            + "    \"name\": \"ModernTag1\",\n"
-            + "    \"tagTypeId\": \"memory\",\n"
-            + "    \"value\": {\n"
-            + "      \"value\": 0,\n"
-            + "      \"quality\": 0,\n"
-            + "      \"timestamp\": \"Nov 2, 2017 8:35:25 AM\"\n"
-            + "    }\n"
-            + "  }\n"
-            + "}";
+
     }
-
-
 }
